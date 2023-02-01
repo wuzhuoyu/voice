852a2970c4da04fb2742c0a2abe4206ac165bc66
wuzhuoyu <wuzhuoyu77@163.com>
2023-02-01 09:58:34 +0800

    init project

---
+++ app/src/main/java/com/yuu/android/sample/voice/SampleActivity.kt
@@ -0,0 +1,11 @@
+package com.yuu.android.sample.voice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SampleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
