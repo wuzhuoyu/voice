b09e31929c271abff06bcaa6f80d4a86f00d379a
leisiyu <2630940292@qq.com>
2023-02-03 10:16:33 +0800

    [version] voice-0.03-alpha

--- voice/build.gradle.kts
+++ voice/build.gradle.kts
@@ -59,7 +59,7 @@ afterEvaluate {
             create<MavenPublication>("release"){
                 group = "com.yuu.android.component"
                 artifactId = "voice"
-                version = "0.0.2-alpha"
+                version = "0.0.3-alpha"
 
                 afterEvaluate { artifact(tasks.getByName("bundleReleaseAar")) }
             }
